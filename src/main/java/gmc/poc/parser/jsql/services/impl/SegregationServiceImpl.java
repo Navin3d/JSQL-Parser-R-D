d8c2db15e36d93a92d3e503c61a324459686a2ff
Navin3d <smnavin65@gmail.com>
2023-05-24 14:14:02 +0530

    Classification if script with regex

--- src/main/java/gmc/poc/parser/jsql/services/impl/SegregationServiceImpl.java
+++ src/main/java/gmc/poc/parser/jsql/services/impl/SegregationServiceImpl.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,55 +38,63 @@ import net.sf.jsqlparser.statement.select.Select;
 @Slf4j
 @Service
 public class SegregationServiceImpl implements SegregationService {
+	// Regex patterns to match different statement types
+	Pattern createPattern = Pattern.compile("^\\s*CREATE", Pattern.CASE_INSENSITIVE);
+	Pattern alterPattern = Pattern.compile("^\\s*ALTER", Pattern.CASE_INSENSITIVE);
+	Pattern insertPattern = Pattern.compile("^\\s*INSERT", Pattern.CASE_INSENSITIVE);
+	Pattern selectPattern = Pattern.compile("^\\s*SELECT", Pattern.CASE_INSENSITIVE);
+	Pattern dropPattern = Pattern.compile("^\\s*DROP", Pattern.CASE_INSENSITIVE);
 
 	@Value("${config.directory.path}")
 	private String baseDir;
-	
+
 	@Autowired
 	private ResourceLoader resourceLoader;
 
 	@Override
 	public void process() {
-		
+
 	}
+
 	List<String> filesFromFolders = new ArrayList<>();
 
 	public List<String> getFileFromDirectory(File directory) {
-		
-    	log.error("==>" + directory.getAbsolutePath());
-        if (directory.isDirectory()) {
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isDirectory()) {
-                    	getFileFromDirectory(file); // Recursively call the method for nested directories
-                    } else {
-                    	filesFromFolders.add(file.getAbsolutePath());
-                    }
-                }
-            }
-        }
-        
-        log.error("ChecK: " + filesFromFolders.toString());
-        return filesFromFolders;
-        
-    }
+
+		log.error("==>" + directory.getAbsolutePath());
+		if (directory.isDirectory()) {
+			File[] files = directory.listFiles();
+			if (files != null) {
+				for (File file : files) {
+					if (file.isDirectory()) {
+						getFileFromDirectory(file); // Recursively call the method for nested directories
+					} else {
+						if (file.getName().contains(".sql"))
+							filesFromFolders.add(file.getAbsolutePath());
+					}
+				}
+			}
+		}
+
+		log.error("ChecK: " + filesFromFolders.toString());
+		return filesFromFolders;
+
+	}
 
 	@Bean(name = "rawSql")
 	public Map<ScriptType, List<SQLModel>> rawSql() throws IOException {
 		Map<ScriptType, List<SQLModel>> returnValue = new HashMap<>();
-		
+
 		Resource resourceBase = resourceLoader.getResource("classpath:" + baseDir + "/");
 		File baseFolder = resourceBase.getFile();
 		List<String> files = getFileFromDirectory(baseFolder);
 		log.error("Not Empty" + files.toString());
-		
+
 		for (String filePath : files) {
 			File resource = new File(filePath);
 			SQLModel sqlModel = new SQLModel();
 			sqlModel.setFileName(resource.getName());
-        	log.error(resource.getName());
-            InputStream inputStream = new FileInputStream(resource);
+			log.error(resource.getName());
+			InputStream inputStream = new FileInputStream(resource);
 			byte[] bData = FileCopyUtils.copyToByteArray(inputStream);
 			String tempScript = (new String(bData, StandardCharsets.UTF_8)).trim();
 			sqlModel.setScript(tempScript);
@@ -102,23 +112,25 @@ public class SegregationServiceImpl implements SegregationService {
 	}
 
 	@Override
-	public ScriptType classifyScripts(String sqlScript) {
-		try {
-			Statement statement = CCJSqlParserUtil.parse(sqlScript);
-			if (statement instanceof CreateTable)
-				return ScriptType.CREATE;
-			else if (statement instanceof Insert)
-				return ScriptType.INSERT;
-			else if (statement instanceof Alter)
-				return ScriptType.ALTER;
-			else if (statement instanceof Select)
-				return ScriptType.SELECT;
-			else
-				return ScriptType.UNKNOWN;
-		} catch (JSQLParserException e) {
-			log.error("Error parsing SQL script: " + e.getMessage());
-		}
-		return ScriptType.UNKNOWN;
+	public ScriptType classifyScripts(String sqlStatement) {
+		ScriptType returnValue = ScriptType.UNKNOWN;
+		// Match the SQL statement against the patterns
+		Matcher createMatcher = createPattern.matcher(sqlStatement);
+		Matcher alterMatcher = alterPattern.matcher(sqlStatement);
+		Matcher insertMatcher = insertPattern.matcher(sqlStatement);
+		Matcher selectMatcher = selectPattern.matcher(sqlStatement);
+		Matcher dropMatcher = dropPattern.matcher(sqlStatement);
+		if (createMatcher.find())
+			returnValue = ScriptType.CREATE;
+		if (insertMatcher.find())
+			returnValue = ScriptType.INSERT;
+		if (alterMatcher.find())
+			returnValue = ScriptType.ALTER;
+		if (selectMatcher.find())
+			returnValue = ScriptType.SELECT;
+		if(dropMatcher.find())
+			returnValue = ScriptType.DROP;
+		return returnValue;
 	}
 
 	@Override
